bce6c3e89f59a1e4fba35ed8ab3b358cdd80aed1
joas124 <joasdavi16@gmail.com>
2024-04-20 13:51:40 +0100

    Changes to DB

--- frontend/src/components/signup.js
+++ frontend/src/components/signup.js
@@ -6,6 +6,9 @@ export default function SignUp(){
     <div className='signup'>
       <div className='signup-div'>
         <h2 className='signup-title'>Sign Up</h2>
+        <Box className='input-field'>
+          <TexTField id='outlined'
+          label=
         <form className='signup-form'>
           <input type='mail' className='signup-input' placeholder='Email' />
           <input type='text' className='signup-input' placeholder='Username' />
