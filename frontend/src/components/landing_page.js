f88a044caac14faabc51caeb2d5cacfe95b3c6b8
BernardoPedro <bernardopedro@student.dei.uc.pt>
2024-04-20 10:43:36 +0100

    logo font navbar

--- frontend/src/components/landing_page.js
+++ frontend/src/components/landing_page.js
@@ -2,24 +2,29 @@ import "./landing_page.css";
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function LandingPage(){
-
-  return(
-    <div className='landing-page'>
-      <div className='text-and-login'>
-        <h2 className='value'>Save, Manage and Share any work/project you have!</h2>
-        <p className='join'>Join Now!</p>
-        <div className='login-register'>
-          <button className='login-landing'>
-            <Link to='/login'>Login</Link>
+export default function LandingPage() {
+  return (
+    <div className="landing-page">
+      <div className="text-and-login">
+        <h2 className="value">
+          Save, Manage and Share any work/project you have!
+        </h2>
+        <p className="join">Join Now!</p>
+        <div className="login-register">
+          <button className="login-landing">
+            <Link to="/login">Login</Link>
           </button>
           <p>OR</p>
-          <button className='signup-landing'>
-            <Link to='/signup'>Sign Up</Link>
+          <button className="signup-landing">
+            <Link to="/signup">Sign Up</Link>
           </button>
         </div>
       </div>
-      <img src="./LogoRepoStudentsemfundo.png"/>
+      <img
+        className="image-landing"
+        src="./happy-blonde-college-student-posing-against-pink-wall.png"
+        alt="Mulher Sorrindo"
+      />
     </div>
   );
 }
